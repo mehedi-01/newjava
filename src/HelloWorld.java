a917c38a2e0fe345cee3bcb9caac578173db9f32
mehedi-01 <mehehedihasan376766@gmail.com>
2022-10-01 23:43:28 +0600

    Initial version

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public  static void main(String[] args){
-        System.out.println("Hello ,Nayeem How are u");
+        System.out.println("Hello , How are u");
 
 
     }
